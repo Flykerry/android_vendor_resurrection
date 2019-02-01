sdhz152/android_vendor_lineage

Add to GooglePinYin

config/common_full_phone.mk
@@ -1,11 +1,20 @@
 # Inherit full common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full.mk)
 
+ifeq ($(SUDA_CPU_ABI),arm64-v8a)
+PRODUCT_PACKAGES += \
+    GooglePinYin
+
+PRODUCT_COPY_FILES += $(shell test -d vendor/lineage/prebuilt/common/app/GooglePinYin && \
+    find vendor/lineage/prebuilt/common/app/GooglePinYin -name '*.so' \
+    -printf '%p:system/app/GooglePinYin/lib/arm64/%f ')
+else
 # Required packages
 PRODUCT_PACKAGES += \
     LatinIME
 
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
+endif
 
 $(call inherit-product, vendor/lineage/config/telephony.mk)

config/common_full_tablet_wifionly.mk
@@ -1,9 +1,18 @@
 # Inherit full common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full.mk)
 
+ifeq ($(SUDA_CPU_ABI),arm64-v8a)
+PRODUCT_PACKAGES += \
+    GooglePinYin
+
+PRODUCT_COPY_FILES += $(shell test -d vendor/lineage/prebuilt/common/app/GooglePinYin && \
+    find vendor/lineage/prebuilt/common/app/GooglePinYin -name '*.so' \
+    -printf '%p:system/app/GooglePinYin/lib/arm64/%f ')
+else
 # Required packages
 PRODUCT_PACKAGES += \
     LatinIME
 
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
+endif

Flykerry/android_vendor_resurrection

config/common_full_phone.mk
@@ -1,6 +1,14 @@
 # Inherit full common Lineage stuff
 $(call inherit-product, vendor/rr/config/common_full.mk)
 
+ifeq ($(SUDA_CPU_ABI),arm64-v8a)
+PRODUCT_PACKAGES += \
+    GooglePinYin
+
+PRODUCT_COPY_FILES += $(shell test -d vendor/rr/prebuilt/common/app/GooglePinYin && \
+    find vendor/rr/prebuilt/common/app/GooglePinYin -name '*.so' \
+    -printf '%p:system/app/GooglePinYin/lib/arm64/%f ')
+else
 # Required packages
 PRODUCT_PACKAGES += \
     LatinIME

config/common_full_tablet_wifionly.mk
@@ -1,6 +1,14 @@
 # Inherit full common Lineage stuff
 $(call inherit-product, vendor/rr/config/common_full.mk)
 
+ifeq ($(SUDA_CPU_ABI),arm64-v8a)
+PRODUCT_PACKAGES += \
+    GooglePinYin
+
+PRODUCT_COPY_FILES += $(shell test -d vendor/rr/prebuilt/common/app/GooglePinYin && \
+    find vendor/rr/prebuilt/common/app/GooglePinYin -name '*.so' \
+    -printf '%p:system/app/GooglePinYin/lib/arm64/%f ')
+else
 # Required packages
 PRODUCT_PACKAGES += \
     LatinIME
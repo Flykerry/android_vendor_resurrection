85b52494bc7d062fe80fb288db0f253f27600bcd
sdhz150 <yanghui150@163.com>
2018-10-09 15:47:05 +0800

    Add to GooglePinYin

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -1,6 +1,14 @@
 # Inherit full common Lineage stuff
 $(call inherit-product, vendor/rr/config/common_full.mk)
 
+ifeq ($(SUDA_CPU_ABI),arm64-v8a)
+PRODUCT_PACKAGES += \
+    GooglePinYin
+
+PRODUCT_COPY_FILES += $(shell test -d vendor/rr/prebuilt/common/app/GooglePinYin && \
+    find vendor/rr/prebuilt/common/app/GooglePinYin -name '*.so' \
+    -printf '%p:system/app/GooglePinYin/lib/arm64/%f ')
+else
 # Required packages
 PRODUCT_PACKAGES += \
     LatinIME
